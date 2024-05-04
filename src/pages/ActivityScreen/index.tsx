07704605edc1faa8c2e6126c7da67db800289b2b
Andor Kesselman <andor@andor.us>
2024-05-04 23:21:34 +0530

    added new activity view

--- src/pages/ActivityScreen/index.tsx
+++ src/pages/ActivityScreen/index.tsx
@@ -1,19 +1,56 @@
 import WorkoutChart from "@/components/workout/WorkoutChart";
 import HomeButton from "@/components/buttons/navigation/HomeButton";
+
 import { useRouter } from "next/router";
 import { getSessions } from "@/lib/store/dwn/session";
-import { WorkoutSession } from "@/models/workout";
+import { WorkoutSession, Step } from "@/models/workout";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/lib/reducers";
+import "./segment.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import ShareIcon from "@mui/icons-material/Share";
 
 type SessionCardProps = {
   session: WorkoutSession;
   index: number;
+  onSelect?: (session: WorkoutSession) => void; // Optional onSelect callback function
+  onDelete?: (session: WorkoutSession) => void;
+  onEdit?: (session: WorkoutSession) => void;
+  onShare?: (session: WorkoutSession) => void;
 };
 
-function SessionCard({ session, index }: SessionCardProps) {
+type StepBarProps = {
+  steps: Step[];
+};
+const ProgressBar: React.FC<StepBarProps> = ({ steps }) => {
+  const totalDuration = steps.reduce((acc, step) => acc + step.duration, 0);
+  return (
+    <div className="progress-bar bg-gray-100">
+      {steps.map((step, index) => (
+        <div
+          key={index}
+          className={`segment ${step.color}`}
+          style={{
+            width: `${(step.duration / totalDuration) * 100}%`,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+function SessionCard({
+  session,
+  index,
+  onEdit,
+  onDelete,
+  onSelect,
+  onShare,
+}: SessionCardProps) {
   const startTime = session.startTime ? new Date(session.startTime) : null;
+  const web5state = useSelector((state: RootState) => state.web5);
   const endTime = session.endTime ? new Date(session.endTime) : null;
   let formattedDuration = "";
 
@@ -35,38 +72,71 @@ function SessionCard({ session, index }: SessionCardProps) {
   }
 
   return (
-    <div className="border border-gray-200 p-4 rounded-md">
-      <div>
-        <span className="font-bold">Session ID: </span> {session?.id}
+    <div className="bg-white shadow-lg rounded-lg p-4 m-4 relative hover:bg-gray-100">
+      <div className="absolute top-4 right-4 flex space-x-2">
+        <div>
+          <button
+            onClick={() => {
+              if (onEdit && session) {
+                onEdit(session);
+              }
+            }}
+          >
+            <EditIcon />
+          </button>
+          <button
+            onClick={() => {
+              if (onDelete && session) {
+                onDelete(session);
+              }
+            }}
+          >
+            <DeleteIcon />
+          </button>
+          <button
+            onClick={() => {
+              if (onShare && session) {
+                onShare(session);
+              }
+            }}
+          >
+            <ShareIcon />
+          </button>
+        </div>
       </div>
       <div>
-        <span className="font-bold">Workout Name:</span>{" "}
-        {session?.routine?.name}
+        <div>
+          <span className="font-bold">Session ID: </span> {session?.id}
+        </div>
+        <div>
+          <span className="font-bold">Workout Name:</span>{" "}
+          {session?.routine.name}
+        </div>
+        <div>
+          <span className="font-bold">Start Time:</span> {session?.startTime}
+        </div>
+        <div>
+          <span className="font-bold">End Time:</span> {session?.endTime}
+        </div>
+        <div>
+          <span className="font-bold">Time Required to Complete:</span>{" "}
+          {formattedDuration}
+        </div>
+        <div>
+          <span className="font-bold">Workout Duration:</span>{" "}
+          {session?.totalTime / 1000} seconds
+        </div>
+        <div>
+          <span className="font-bold">Number of Steps:</span>{" "}
+          {session?.steps?.length}
+        </div>
+        <div>
+          <span className="font-bold">Completed:</span>{" "}
+          {session?.completed ? "Yes" : "No"}
+        </div>
       </div>
-      <div>
-        <span className="font-bold">Workout ID:</span> {session?.routine?.id}
-      </div>
-      <div>
-        <span className="font-bold">Start Time:</span> {session?.startTime}
-      </div>
-      <div>
-        <span className="font-bold">End Time:</span> {session?.endTime}
-      </div>
-      <div>
-        <span className="font-bold">Time Required to Complete:</span>{" "}
-        {formattedDuration}
-      </div>
-      <div>
-        <span className="font-bold">Workout Duration:</span>{" "}
-        {session?.totalTime}
-      </div>
-      <div>
-        <span className="font-bold">Number of Steps:</span>{" "}
-        {session?.steps?.length}
-      </div>
-      <div>
-        <span className="font-bold">Completed:</span>{" "}
-        {session?.completed ? "Yes" : "No"}
+      <div className="">
+        {session.steps && <ProgressBar steps={session.steps} />}
       </div>
     </div>
   );
@@ -90,7 +160,7 @@ export default function ActivityPage() {
   // @ts-ignore
   const loadSessions = async (web5?: Web5 | null) => {
     if (!web5) {
-      throw new Error("can't load routines. web5 not loaded");
+      throw new Error("can't load sessions. web5 not loaded");
     }
     const sessions = await getSessions(web5);
     const data: WorkoutSession[] = await Promise.all(
@@ -108,14 +178,28 @@ export default function ActivityPage() {
         <HomeButton />
       </div>
       <div className="flex justify-center items-center">
-        This page is not yet active yet. We are working on it!
         <br />
         Workout History
       </div>
       <div className="space-y-4">
         {sessions.map((session, index) => (
-          <div key={index} className="border border-gray-200 p-4 rounded-md">
-            <SessionCard session={session} index={index} />
+          <div key={index} className=" rounded-md">
+            <SessionCard
+              session={session}
+              index={index}
+              onSelect={(w: WorkoutSession) => {
+                alert("not supported yet");
+              }}
+              onEdit={(w: WorkoutSession) => {
+                alert("not supported yet");
+              }}
+              onDelete={(w: WorkoutSession) => {
+                alert("not supported yet");
+              }}
+              onShare={(w: WorkoutSession) => {
+                alert("not supported yet");
+              }}
+            />
           </div>
         ))}
       </div>
