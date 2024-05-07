c542e6d60bd6a7c9b61cca4964562ecff09fb910
Andor Kesselman <andor@andor.us>
2024-05-07 09:24:51 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -36,6 +36,26 @@ const nextConfig = {
     buildExcludes: [/middleware-manifest.json$/],
   },
   webpack: (config, { isServer }) => {
+    const registerJs = path.join(
+      path.dirname(require.resolve("next-pwa")),
+      "register.js",
+    );
+    const entry = config.entry;
+
+    config.entry = () =>
+      entry().then((entries) => {
+        // Automatically registers the SW and enables certain `next-pwa` features in
+        // App Router (https://github.com/shadowwalker/next-pwa/pull/427)
+        if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+          if (Array.isArray(entries["main-app"])) {
+            entries["main-app"].unshift(registerJs);
+          } else if (typeof entries["main-app"] === "string") {
+            entries["main-app"] = [registerJs, entries["main-app"]];
+          }
+        }
+        return entries;
+      });
+
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
